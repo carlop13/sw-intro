7553fae2758da733baf34f94af5b25be2c9c9e7e
carlop13 <carlosguadalupelt13@gmail.com>
2024-09-23 17:59:05 -0600

    creacion de appShell

--- sw.js
+++ sw.js
@@ -1,24 +1,14 @@
-self.addEventListener('install', event => {
+//Instalación
+self.addEventListener('install', (event) => {
 
-    //respuesta offline
-    const offline = new Response(`
-        <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite + React</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <h1>Estás offline</h1>
-  </body>
-</html> `, {
-    headers: { 'Content-Type': 'text/html' }
-});
-        const resp = fetch(event.request)
-        .catch(() => offline)
+})
 
-        event.respondWith(resp)
+//Activacion
+self.addEventListener('activate', (event) => {
+
+})
+
+//Instalación
+self.addEventListener('fetch', (event) => {
+
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    viteStaticCopy({
+      targets:[ {
+        src: "sw.js",
+        dest: ""
+      },
+      {
+        src: "js/app.js",
+        dest: "js"
+      }]
+    }),
+    react()
+  ],
+});
