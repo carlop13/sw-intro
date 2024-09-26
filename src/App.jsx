596dc76452a0ce030075218c77d4d0f6bf4b7bd3
carlop13 <carlosguadalupelt13@gmail.com>
2024-09-26 17:57:11 -0600

    cacheD

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Card, Col, Row } from 'antd';
 import img1 from './assets/boston1.jpg'
 import img2 from './assets/boston2.jpeg'
 import img3 from './assets/boston3.jpeg'
+import img4 from './assets/perrolentes.JFIF'
+import img5 from './assets/cielo.JFIF'
+import img6 from './assets/saltar.JFIF'
 
 const { Meta } = Card;
 
@@ -13,6 +16,7 @@ import './App.css'
 function App() {
 
   return (
+    <>
     <Row gutter={16}>
       <Col span={8}>
         <Card
@@ -48,6 +52,43 @@ function App() {
         </Card>
       </Col>
     </Row>
+
+<Row gutter={16}>
+<Col span={8}>
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img src={img4} />}
+  >
+    <Meta title="Boston café" description="www.instagram.com" />
+  </Card>
+</Col>
+<Col span={8}>
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img src={img5} />}
+  >
+    <Meta title="Boston en la nieve" description="www.instagram.com" />
+  </Card>
+</Col>
+<Col span={8}>
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img src={img6} />}
+  >
+    <Meta title="Boston en otoño" description="www.instagram.com" />
+  </Card>
+</Col>
+</Row>
+</>
   )
 }
 
